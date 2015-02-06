bae282be2731133106d8662e4cc91f08c223f1e5
wolfgangleon <wolfgangrleonh@gmail.com>
2015-02-06 12:55:16 -0500

    alert message

--- js/script.js
+++ js/script.js
@@ -2,13 +2,21 @@ $(document).ready( function () {
 	console.log('Wleonproduction@gmail.com  Miami Florida');
 
 	//SCRIPT ADD ITEM\ 
+	
 	$('.add-item, .add-qty').keydown(function(e) {
 		if (e.which == 13) {
 			//console.log('pressed enter')
+
+			if ( $('.add-item').val()=='' ) {	
+				alert('Please add an item first');
+				$('.add-item, .add-qty').val('');
+				return false;};
+
 			var  listname = $('.add-item').val();
 			var  listqty = $('.add-qty').val();
 			$('.clear').show();
 			$('.items').prepend('<li class="item"><span class="name">'+listname+'</span><span class="quantity">'+listqty+'</span><div class="controls"><img class="delete" src="images/x.png" alt=""></div></li>'); 
+			$('.add-item, .add-qty').val('');
 		} 
 	});// END SCRIPT ADD-ITEM & ADD-QTY
 
